946f4c18103258a8155356e7ae94609be4f627ee
andfla98 <160551146+andfla98@users.noreply.github.com>
2024-05-05 09:02:02 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,18 @@
+const textoArea = document.getElementById("texto");
+const resultadoCaracteres = document.getElementById("numCaracteres");
+const resultadoPalavras = document.getElementById("numPalavras");
+const resultadoEspacos = document.getElementById("numEspacos");
+
+textoArea.addEventListener("input", () => {
+    const texto = textoArea.value;
+    
+    const numCaracteres = texto.replace(/\s/g, '').length;
+    resultadoCaracteres.textContent = numCaracteres;
+
+    const palavras = texto.match(/\S+/g) || [];
+    const numPalavras = palavras.length;
+    resultadoPalavras.textContent = numPalavras;
+
+    const numEspacos = texto.match(/ /g)?.length || 0;
+    resultadoEspacos.textContent = numEspacos;
+});
